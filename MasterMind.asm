4bb8ec85cf364c6201e33e1ea2566670053d61d5
Tiago Costa Chousal <2024116801@ufp.edu.pt>
2025-11-18 17:25:55 +0000

    Inicio da matriz

--- MasterMind.asm
+++ MasterMind.asm
@@ -14,7 +14,37 @@ MSGDerrota1: .asciiz "Oh que pena. Nao acertaste a cobinação ao fim de 10 tentat
 MSGDerrota2: .asciiz "A sequencia correta de cores era: \n"
 MSGPontuacao: .asciiz "Pontuação: \n"
 MSGSettings1: .asciiz "Nesta parte é possível adicionar cores duplicadas ou remover Cores\n" # Depois vejo melhor essa parte
+Matriz: .word 0:40
+MatrizLinhas: .word 10
+MatrizColunas: .word 4
 .text
 .globl main
 
 main:
+
+	# endereços para se utilizar na matriz #
+	
+	add $s0, $0, $0 # i -> linhas
+	add $s1, $0, $0 # j -> colunas
+	lw $s2, MatrizColunas
+	la $t0, Matriz
+	
+LoopI:
+	beq $s0, 10, Exit
+
+LoopJ:
+	beq $s1, 4, LoopIncrement
+	mul $t1, $s0, $s2			# Cálculo: offset = [(i * 4) + j] * 4 onde a 4 seria o sll
+	add $t1, $t1, $s1
+	sll $t1, $t0, 2
+	add $t2, $t1, $t0
+	sw $s0, 0($t2)
+	addi $s1, $s1, 1
+	j LoopJ
+
+LoopIncrement:
+	move $0, $s1
+	addi $s0, $s0, 1
+	j LoopI
+
+Exit:
